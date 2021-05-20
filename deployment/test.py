b3bf2a5dde273108af28fb39b5ea889b6724d2d9
Nazmus Sakib <52566550+sns-sakib@users.noreply.github.com>
2021-05-21 01:47:29 +0600

    update test.py

--- deployment/test.py
+++ deployment/test.py
@@ -1,2 +1,20 @@
-model_path = "./densenet121 male female pa rsna normal and pneumonea 11 epochs train auc 96% test auc 67% last to 14_14 layer unfreeze_new augmentation_zoom.h5"
-model = models.load_model(model_path, backbone_name='densenet121', convert=True, nms=False)
+from PIL import Image
+import numpy as np
+from skimage import transform
+from keras.models import load_model
+
+def load(filename):
+   np_image = Image.open(filename)
+   np_image = np.array(np_image).astype('float32')/255
+   np_image = transform.resize(np_image, (224, 224, 3))
+   np_image = np.expand_dims(np_image, axis=0)
+   return np_image
+
+def test(image_path):
+  #output pred_score returns an array like this [[9.256325e-08, 0.9999999]] where the first value 0 (not pneumonia), second value is 1 (pneumonia)
+  #load model
+   model_path = "./densenet121 male female pa rsna normal and pneumonea 11 epochs train auc 96% test auc 67% last to 14_14 layer unfreeze_new augmentation_zoom.h5"
+   model = models.load_model(model_path)
+   image = load(image_path)
+   pred_score = model.predict(image)
+   return pred_score
